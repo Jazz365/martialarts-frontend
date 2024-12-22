90664c39aeb1a3bf55b5eb9fa5a1ec4729626569
Ayoola <ayo.oloyede16@gmail.com>
2024-12-22 14:38:57 +0100

    Revisions v5

--- features/Places/components/SinglePlaceLocation/SinglePlaceLocation.tsx
+++ features/Places/components/SinglePlaceLocation/SinglePlaceLocation.tsx
@@ -1,16 +1,22 @@
+'use client';
+
 import React from 'react'
 import styles from './styles.module.css'
 import PlacesMap from '@/features/Search/sections/Map/Map'
+import useMobile from '@/hooks/useMobile';
 
 
 const SinglePlaceLocation = () => {
+    const isMobile = useMobile();
+
     return (
         <section className={styles.content__Wrap}>
             <h3 className={styles.header}>location</h3>
             
             <PlacesMap 
-                minHeight='24rem'
+                minHeight={isMobile ? '14rem' : '24rem'}
                 width='100%'
+                showContentOnSmallScreen={true}
             />
         </section>
     )
