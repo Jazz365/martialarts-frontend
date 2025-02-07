7a841e7e141ea9a29580e3fcb5e2917c6c0fb94e
Ayoola <ayo.oloyede16@gmail.com>
2025-02-07 16:45:11 +0100

    update v1

--- app/api/map-detail/route.ts
+++ app/api/map-detail/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
     return NextResponse.json({
         message: 'Successfully fetched detail!',
-        data: process.env.MAP_KEY,
+        data: process.env,
     }, {
         status: 200,
     })
